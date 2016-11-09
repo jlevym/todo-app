6757d523f4f9bae28bb6ac5345c13d0535566e49
jlevym <jlevym@gmail.com>
2016-11-09 19:58:48 +0200

    impemented get and post from the scotch tutorial

--- app.js
+++ app.js
@@ -6,10 +6,14 @@ var setupController = require('./controllers/setupController');
 var apiController   = require('./controllers/apiController');
 
 
+
+
 var port = process.env.PORT|| 3000;
 
 app.set('view engine', 'ejs');
-app.use('/assets', express.static(__dirname + '/public'));
+//app.use('/assets', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res) {
     res.render('index');
 });
@@ -19,4 +23,9 @@ mongoose.connect(config.getDbConnectionString());
 setupController(app); // the function to generate seed data if app/setupTodos
 apiController(app);   // the function containing the api endpoints for add, delete, get
 
+app.get('*', function(req, res) {
+        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+
 app.listen(port);
+console.log('listening on port: ' + port);
--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,16 +1,24 @@
 // this module will house all the api endpoints
 var Todos = require('../models/todoModel');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var morgan = require('morgan');
+
 
 module.exports = function(app) {
 
-  app.use(bodyParser.json());
+    app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+    app.use(methodOverride());
+    app.use(morgan('dev'));
 
     // get all the todos of a specific user
-    app.get('/api/todos/:uname', function(req, res) {
+    app.get('/api/todos', function(req, res) {
+    // app.get('/api/todos/:uname', function(req, res) {        
 
-        Todos.find({ username: req.params.uname }, function(err, todos) {
+        //Todos.find({ username: req.params.uname }, function(err, todos) {
+        Todos.find({}, function(err, todos) {
             if (err) throw err;
 
             res.send(todos);
@@ -29,7 +37,7 @@ module.exports = function(app) {
         
     });
     
-    app.post('/api/todo', function(req, res) {
+  /*  app.post('/api/todo', function(req, res) {
         if (req.body.id) {
             Todos.findByIdAndUpdate(req.body.id, { todo: req.body.todo, isDone: req.body.isDone, hasAttachment: req.body.hasAttachment }, function(err, todo) {
                 if (err) throw err;
@@ -39,12 +47,12 @@ module.exports = function(app) {
         }
         
        else {
-           
+           console.log(req);
            var newTodo = Todos({
-               username: 'jeffrey',
+              username: 'jeffrey',
                todo: req.body.todo,
-               isDone: req.body.isDone,
-               hasAttachment: req.body.hasAttachment
+             isDone: req.body.isDone,
+              hasAttachment: req.body.hasAttachment
            });
            newTodo.save(function(err) {
                if (err) throw err;
@@ -53,7 +61,31 @@ module.exports = function(app) {
             
         }
         
+    });*/
+
+// create todo and send back all todos after creation
+    app.post('/api/todo', function(req, res) {
+
+        // create a todo, information comes from AJAX request from Angular
+        Todos.create({
+            todo : req.body.todo,
+            isDone : false
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            Todos.find(function(err, todos) {
+                if (err)
+                    res.send(err)
+                res.json(todos);
+            });
+        });
+
     });
+
+
+
 
     // delete a todo
     app.delete('/api/todo', function(req, res) {
